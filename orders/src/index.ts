41ede98db9cabe2a9311ed43f07fd0f8e08debb8
Shyamnath Vishwakarma <svishwakarma@3clogic.com>
2020-08-03 17:28:12 +0530

    updated start message

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -24,7 +24,7 @@ const start = async () => {
   }
 
   try {
-    console.log('Starting');
+    console.log('Starting ....');
     await natsWrapper.connect(
       process.env.NATS_CLUSTER_ID,
       process.env.NATS_CLIENT_ID,
